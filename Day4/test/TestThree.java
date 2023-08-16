f67e0b52fdc43e4a24ba3c0d6055b0d08e493166
PoojaRagi <113049414+PoojaRagi@users.noreply.github.com>
2023-08-16 12:50:47 +0530

    Add files via upload

---
+++ Day4/test/TestThree.java
@@ -0,0 +1,14 @@
+package test;
+import demo.TestOne;
+
+public class TestThree extends TestOne{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		TestThree t3=new TestThree();
+		t3.id=5;
+		System.out.println(t3.id);
+		t3.display();
+
+	}
+}
