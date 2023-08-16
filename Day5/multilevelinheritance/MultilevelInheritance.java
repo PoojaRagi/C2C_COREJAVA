bfe20c09f0cb5b4a5fb863617aff694998dd537f
PoojaRagi <113049414+PoojaRagi@users.noreply.github.com>
2023-08-16 13:11:55 +0530

    Add files via upload

---
+++ Day5/multilevelinheritance/MultilevelInheritance.java
@@ -0,0 +1,18 @@
+package multilevelinheritance;
+
+public class MultilevelInheritance {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		City c1=new City();
+		c1.setCityName("Bagalkot");
+		c1.setPinCode(587102);
+		c1.setStateName("Karnataka");
+		c1.setLanguage("Kannada");
+		c1.setCountryName("India");
+		c1.setCapital("Delhi");
+		System.out.println(c1);		
+
+	}
+
+}
---
+++ Day5/singleinheritance/SingleInheritance.java
@@ -0,0 +1,12 @@
+package singleinheritance;
+
+public class SingleInheritance {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Student s1=new Student("Pooja","19-02-2007","Bagalkot","123456789098L",1234567890,1,"XYZ");
+		System.out.println(s1);
+
+	}
+
+}
